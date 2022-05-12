f72df4a1ed92310ab2ee2e0d3530860f5e90eb65
Mauro Hendriks <89855665+Mauro-Hendriks@users.noreply.github.com>
2022-05-12 13:59:46 +0200

    fixed bug
    fixed collision bug and no more wall jump and doudle jumping

--- Unity/BO Expanding Space/Assets/scripts/movement.cs
+++ Unity/BO Expanding Space/Assets/scripts/movement.cs
@@ -8,9 +8,11 @@ public class movement : MonoBehaviour
     public Rigidbody2D player;
     public float jumpHeight = 10;
     bool onGround;
-    
-    
-   
+    private Vector2 validDirection = Vector2.right;
+    private Vector2 validDirectionS = Vector2.left;
+    private float contactThreshold = 30;
+
+
     void Start()
     {
         
@@ -30,13 +32,33 @@ public class movement : MonoBehaviour
         if (Input.GetKey(KeyCode.W)&& onGround==true)
         {
             player.AddForce(new Vector2(0, jumpHeight)* Time.deltaTime, ForceMode2D.Impulse);
-            
         }  
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         onGround = true;
+
+        if (collision.gameObject.name == "Platform")
+        {
+            for (int k = 0; k < collision.contacts.Length; k++)
+            {
+                if (Vector2.Angle(collision.contacts[k].normal, validDirection) <= contactThreshold)
+                {
+                    onGround = false;
+                    break;
+                }
+            }
+            for (int k = 0; k < collision.contacts.Length; k++)
+            {
+                if (Vector2.Angle(collision.contacts[k].normal, validDirectionS) <= contactThreshold)
+                {
+                    onGround = false;
+                    break;
+                }
+            }
+        }
     }
+
 
     private void OnCollisionExit2D(Collision2D collision)
     {

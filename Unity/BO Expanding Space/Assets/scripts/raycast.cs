67e4d3c011abb59a1b0de47f2394ea6b44a5852c
Mauro Hendriks <89855665+Mauro-Hendriks@users.noreply.github.com>
2022-06-22 12:10:20 +0200

    adde the offset for raycast

--- Unity/BO Expanding Space/Assets/scripts/raycast.cs
+++ Unity/BO Expanding Space/Assets/scripts/raycast.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class raycast : MonoBehaviour
 {
     public LayerMask layermask;
+    public int offset1 = 10000;
 
 
     void Start()
     {
-        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, -Vector2.up, Mathf.Infinity, layermask); ;
+        RaycastHit2D hitinfo = Physics2D.Raycast(origin: transform.position + transform.up * offset1, -Vector2.up, Mathf.Infinity, layermask);
+        Debug.DrawRay(transform.position, -Vector2.up, Color.red, 2);
         transform.position = hitinfo.point;
     }
 }
